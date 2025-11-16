df320d31a15988719e5ca8ea2136a4c02cffd16f
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-11-16 21:55:52 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,21 @@
+
+/**
+ * @param {number} length
+ * @param {number[][]} updates
+ * @return {number[]}
+ */
+var getModifiedArray = function (length, updates) {
+    const valuesAfterAllUpdates = new Array(length).fill(0);
+    for (let [startIndex, endIndex, value] of updates) {
+        valuesAfterAllUpdates[startIndex] += value;
+        if (endIndex + 1 < length) {
+            valuesAfterAllUpdates[endIndex + 1] -= value;
+        }
+    }
+
+    for (let i = 1; i < length; ++i) {
+        valuesAfterAllUpdates[i] += valuesAfterAllUpdates[i - 1];
+    }
+
+    return valuesAfterAllUpdates;
+};
